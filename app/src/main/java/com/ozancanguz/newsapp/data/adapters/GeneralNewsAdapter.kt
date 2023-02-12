7ce8fb730223839d56dd90e566ba557807d7538e
Ozan <ozancangu24@gmail.com>
2023-02-12 20:38:14 +0300

    -news screen re-designed

--- app/src/main/java/com/ozancanguz/newsapp/data/adapters/GeneralNewsAdapter.kt
+++ app/src/main/java/com/ozancanguz/newsapp/data/adapters/GeneralNewsAdapter.kt
@@ -11,6 +11,7 @@ import com.ozancanguz.newsapp.data.model.NewsResult
 import com.ozancanguz.newsapp.ui.fragments.news.NewsFragmentDirections
 import com.ozancanguz.newsapp.utils.Util.Companion.loadImage
 import kotlinx.android.synthetic.main.generalnews_rowlayout.view.*
+import kotlinx.android.synthetic.main.rv.view.*
 
 class GeneralNewsAdapter:RecyclerView.Adapter<GeneralNewsAdapter.GeneralViewHolder>() {
     class GeneralViewHolder(view: View):RecyclerView.ViewHolder(view){
@@ -26,18 +27,19 @@ class GeneralNewsAdapter:RecyclerView.Adapter<GeneralNewsAdapter.GeneralViewHold
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GeneralViewHolder {
         val inflater=LayoutInflater.from(parent.context)
-        val view=inflater.inflate(R.layout.generalnews_rowlayout,parent, false)
+        val view=inflater.inflate(R.layout.rv,parent, false)
         return GeneralViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: GeneralViewHolder, position: Int) {
        val currentNews=generalNews[position]
-        holder.itemView.datetv.text=currentNews.date
-        holder.itemView.sourcetv.text=currentNews.source
-        holder.itemView.nametv.text=currentNews.name
 
+        // news name
+        holder.itemView.rv_nametv.text=currentNews.name
         // glide image loading
-        holder.itemView.generalNewsimageView.loadImage(currentNews.image)
+        holder.itemView.rv_img.loadImage(currentNews.image)
+
+
 
         // navigation safe args
         holder.itemView.setOnClickListener {
