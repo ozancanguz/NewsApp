fa4efcf8f80a38359b1668b542d238ea2559c339
Ozan <ozancangu24@gmail.com>
2023-02-12 17:18:13 +0300

    -newsviewmodel created and injected

---
+++ app/src/main/java/com/ozancanguz/newsapp/viewmodel/NewsViewModel.kt
@@ -0,0 +1,34 @@
+package com.ozancanguz.newsapp.viewmodel
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.ozancanguz.newsapp.data.model.NewsResult
+import com.ozancanguz.newsapp.repository.Repository
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class NewsViewModel@Inject constructor(private val repository: Repository, application: Application):AndroidViewModel(application) {
+
+    val newsList=MutableLiveData<NewsResult>()
+
+    fun getNews(country:String,tag:String){
+        viewModelScope.launch {
+            val response=repository.remote.getNews(country,tag)
+            if(response.isSuccessful){
+
+                newsList.postValue(response.body())
+            }else{
+                Log.d("newsviewmodel","No data")
+            }
+        }
+
+    }
+
+
+}
