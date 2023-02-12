18797407021521d1f6373ca81ca9f5c31f4414a8
Ozan <ozancangu24@gmail.com>
2023-02-12 17:09:02 +0300

    -NewsApi.kt created

---
+++ app/src/main/java/com/ozancanguz/newsapp/data/api/NewsApi.kt
@@ -0,0 +1,23 @@
+package com.ozancanguz.newsapp.data.api
+
+import com.ozancanguz.newsapp.data.model.NewsResult
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface NewsApi {
+
+    // get general news
+    @Headers(
+        "authorization: apikey 6XBClsiyFUwn61jEKgPcWy:7hBFCMRuNZto9F4nzaj3Mz",
+        "content-type: application/json"
+    )
+    @GET("getNews")
+    suspend fun getNews(@Query("country")country:String,
+                        @Query("tag")tag:String
+                        ):Response<NewsResult>
+
+
+
+}
